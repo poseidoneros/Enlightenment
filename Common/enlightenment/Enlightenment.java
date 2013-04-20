0e77587b6e7a0693c4bff66a9ee66946da5eed79
poseidoneros <ap.jones@ntlworld.com>
2013-04-20 12:01:24 +0100

    inital commit

---
+++ Common/enlightenment/Enlightenment.java
@@ -0,0 +1,34 @@
+package enlightenment;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import enlightenment.lib.Reference;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME , version = Reference.VERSION)
+
+public class Enlightenment
+{
+@PreInit
+public void preInit(FMLPreInitializationEvent event)
+{
+    
+}
+    
+@Init
+public void Init(FMLInitializationEvent event)
+{
+    
+}
+    
+@PostInit
+public void PostInit(FMLPostInitializationEvent event)
+{
+    
+}
+
+}
---
+++ Common/enlightenment/lib/Reference.java
@@ -0,0 +1,9 @@
+package enlightenment.lib;
+
+public class Reference
+{
+    public static final String MOD_ID = "Enlightenment";
+    public static final String MOD_NAME = "Enlightenment";
+    public static final String VERSION = "0.0.1";
+    
+}
